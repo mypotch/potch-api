7eeb463900cccf2374c1c6facb516cda78426d84
五月的风 <578595193@qq.com>
2019-01-22 19:44:02 +0800

    .

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,6 +1,14 @@
 class View < ApplicationRecord
   belongs_to :author, polymorphic: true
-  belongs_to :viewable, polymorphic: true,
-             after_add: Proc.new { |view, viewable| viewable.counter.increment!(:comments_count) },
-             after_remove: Proc.new { |view, viewable| viewable.counter.decrement!(:comments_count) }
+  belongs_to :viewable, polymorphic: true
+
+  private
+
+  def before_create
+    self.viewable.counter.increment!(:comments_count)
+  end
+
+  def before_destroy
+    self.viewable.counter.decrement!(:comments_count)
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,14 @@
 class Vote < ApplicationRecord
   belongs_to :author, polymorphic: true
-  belongs_to :voteable, polymorphic: true,
-             after_add: Proc.new { |vote, voteable| voteable.counter.increment!(:comments_count) },
-             after_remove: Proc.new { |vote, voteable| voteable.counter.decrement!(:comments_count) }
+  belongs_to :voteable, polymorphic: true
+
+  private
+
+  def before_create
+    self.voteable.counter.increment!(:comments_count)
+  end
+
+  def before_destroy
+    self.voteable.counter.decrement!(:comments_count)
+  end
 end
